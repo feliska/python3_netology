22af9e85f82ab909c0a707b9f31cf59638cbe494
Feliska <feliska@me.com>
2017-01-16 01:30:25 +0200

    Changed reading from the file. Now program reads from .json (instead .txt). Added cookbook in json format. Input of the dishes. Now you can add as many as you want.

--- homework_cookbook.py
+++ homework_cookbook.py
@@ -1,55 +1,34 @@
-import re
-
-# компиляция выражения для удаления лишних пробельных символов
-reg = re.compile('^\s|\s$')
-
-with open('cook_book.txt') as cookbook:
-    cook_book = {}
-    for line in cookbook:
-        ingredients = []
-        dish = line.lower().strip()
-        ingredients_count = int(cookbook.readline())
-        cook_book[dish] = {'ingredients': ingredients}
-
-        for count in range(ingredients_count):
-            ingr_line = cookbook.readline().split('|')
-            ingredients_of_dish = []
-            # заменила 2 реплейса на цикл с удалением ненужных знаков по регулярке
-            for i in ingr_line:
-                ingr = reg.sub('', i)
-                ingredients_of_dish.append(ingr)
-            includes = {
-                'product': ingredients_of_dish[0],
-                'quantity': int(ingredients_of_dish[1]),
-                'unit': ingredients_of_dish[2]
-            }
-            ingredients.append(includes)
-
-        blank_line = cookbook.readline()
+import json
 
 
-def get_shop_list_by_dishes(dishes, people_count):
-    shop_list = {}
-    for dish in dishes:
-        for ingridient in dish['ingredients']:
-            new_shop_item = dict(ingridient)
-            # пересчитали ингрединты по количеству людей
-            new_shop_item['quantity'] = new_shop_item['quantity'] * people_count
-            if new_shop_item['product'] not in shop_list:
-                shop_list[new_shop_item['product']] = new_shop_item
-            else:
-                shop_list[new_shop_item['product']]['quantity'] += new_shop_item['quantity']
-    return shop_list
+with open("cook_book.json", 'r') as cook:
+    cook_book = json.load(cook)
+
+    def get_shop_list_by_dishes(dishes, people_count):
+        shop_list = {}
+        for dish in dishes:
+            for ingredient in dish['ingredients']:
+                new_shop_item = dict(ingredient)
+                # пересчитали ингрединты по количеству людей
+                new_shop_item['quantity'] = new_shop_item['quantity'] * people_count
+                if new_shop_item['product'] not in shop_list:
+                    shop_list[new_shop_item['product']] = new_shop_item
+                else:
+                    shop_list[new_shop_item['product']]['quantity'] += new_shop_item['quantity']
+        return shop_list
+
 
 def print_shop_list(shop_list):
     for key, shop_list_item in shop_list.items():
         print("{product} {quantity} {unit}".format(**shop_list_item))
+
 
 def create_shop_list(people_count, dishes):
     #заполнили список покупок
     shop_list = get_shop_list_by_dishes(dishes, people_count)
     # Вывести список покупок
     print_shop_list(shop_list)
+
 
 dishes = []
 print('Введите название блюда. Для окончания выбора введите "exit"')
