6cd769159b494e81dffc0ac50d45e630a0125bb1
Feliska <feliska@me.com>
2017-01-25 02:11:33 +0200

    Добавлен запуск 4х процессов обработки фото

--- image_size_change(lesson_2.5)/image_resizer.py
+++ image_size_change(lesson_2.5)/image_resizer.py
@@ -1,11 +1,17 @@
 import os.path
 import subprocess
 import glob
+from multiprocessing import Pool
+
 
 source = 'Source'
 result = 'Result'
 
 images = glob.glob(os.path.join(source, "*.jpg"))
+
+
+def resize_img(filename):
+    subprocess.run(['sips', '--resampleWidth', '200', filename])
 
 # Создание папки "Result", если она не существует
 try:
@@ -17,6 +23,8 @@ for f in images:
     subprocess.run(['cp', f, result])
 
 ready_img = glob.glob(os.path.join(result, "*.jpg"))
-
-for img in ready_img:
-    subprocess.run(['sips', '--resampleWidth', '200', img])
+# parallel processing
+pool = Pool(processes=4)
+pool.map(resize_img, ready_img)
+pool.close()
+pool.join()
