1d8ce95fdd606d7beccb2e582e82b3d1e35bbadb
Feliska <feliska@me.com>
2017-02-15 03:40:09 +0200

    Добавление домашнего задания к уроку 3.5. Работа с API Yandex Метрика

--- Yandex_Metrika(lesson_3.5)/yandex_py_netology.py
+++ Yandex_Metrika(lesson_3.5)/yandex_py_netology.py
@@ -13,3 +13,49 @@ auth_data = {
 # print('?'.join((autorize_url, urlencode(auth_data))))
 
 TOKEN = 'AQAAAAAO7hI3AAQPo12_zUyJokYGnWdPljlpEKs'
+
+
+class YandexMetrika(object):
+    _METRIKA_STAT_URL = 'https://api-metrika.yandex.ru/stat/v1/'
+    _METRIKA_MANAGEMENT_URL = 'https://api-metrika.yandex.ru/management/v1/'
+    token = None
+
+    def __init__(self, token):
+        self.token = token
+
+    def get_header(self):
+        return {
+            'Content-Type': 'application/x-yametrika+json',
+            'Authorization': 'OAuth {}'.format(self.token),
+            'User-Agent': 'Chrome'
+        }
+
+    @property
+    def counter_list(self):
+        url = urljoin(self._METRIKA_MANAGEMENT_URL, 'counters')
+        headers = self.get_header()
+        response = requests.get(url, headers=headers)
+        counter_list = [c['id'] for c in response.json()['counters']]
+        return counter_list
+
+    def get_visits_count(self, counter_id):
+        url = urljoin(self._METRIKA_STAT_URL, 'data')
+        headers = self.get_header()
+        params = {
+            'id': counter_id,
+            'metrics': 'ym:s:visits'
+        }
+        response = requests.get(url, params, headers=headers)
+        pprint(response.json())
+        visits_count = response.json()['data'][0]['metrics'][0]
+        return visits_count
+
+
+metrika = YandexMetrika(TOKEN)
+print(YandexMetrika.__dict__)
+print(metrika.__dict__)
+
+print(metrika.counter_list)
+
+for counter in metrika.counter_list:
+    print(metrika.get_visits_count(counter))
