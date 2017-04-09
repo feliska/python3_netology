76d9df5770da0ce43b870a5b1e7045c3b508241e
Feliska <feliska@me.com>
2017-04-09 21:27:07 +0200

    добавление функции обращения к  api и оформление констант

--- Diploma/first_part.py
+++ Diploma/first_part.py
@@ -3,6 +3,16 @@ import json
 import time
 from collections import Counter
 
+VERSION = '5.63'
+API_ADDRESS = 'https://api.vk.com/method/'
+ACCESS_TOKEN = '76e01e83604e3f9e6527ab13fb0320d4cefd4fa8424a93c3f6032c1f94096f9d09b7fd1bcd0954a9b54db'
+
+
+# запрос к API
+def api_request(method_name, params):
+    response = requests.get(API_ADDRESS + method_name, params).json()
+    return response['response']
+
 
 # деление списка на части
 def list_div(list_for_split, n=25):
@@ -11,10 +21,9 @@ def list_div(list_for_split, n=25):
 
 # получение списка друзей по id
 def friends_get(id_of_user):
-    params['user_id'] = id_of_user
-    response = requests.get(api_address + 'friends.get', params)
+    params = {'user_id': id_of_user, 'access_token': ACCESS_TOKEN, 'v': VERSION}
     try:
-        friends_list = response.json()['response']['items']
+        friends_list = api_request('friends.get', params)['items']
         print("Количество друзей:", len(friends_list))
     except KeyError:
         friends_list = []
@@ -24,12 +33,10 @@ def friends_get(id_of_user):
 
 # получение количества подписчиков и их списка
 def followers_get(id_of_user):
-    params['user_id'] = id_of_user
-    params['count'] = 1000
-    response = requests.get(api_address + 'users.getFollowers', params).json()
-    followers_count = response['response']['count']
+    params = {'user_id': id_of_user, 'access_token': ACCESS_TOKEN, 'v': VERSION, 'count': 1000}
+    followers_count = api_request('users.getFollowers', params)['count']
     print("Общее количество подписчиков:", followers_count)
-    followers_list = response['response']['items']
+    followers_list = api_request('users.getFollowers', params)['items']
     all_followers.extend(followers_list)
     return all_followers
 
@@ -40,9 +47,10 @@ def get_followers_groups(full_list_of_users):
     all_followers_count = len(full_list_of_users)
     merge_list = []
     user_count = 0
-    method = 'execute?access_token=' + access_token + '&code='
+    method = 'execute?access_token=' + ACCESS_TOKEN + '&code='
     print("Обработка групп, в которых состоят подписчики ...")
     for l in divided_list:
+        params = ''
         code = 'return ['
         user_count += 25
         remain_to_calc = all_followers_count - user_count
@@ -53,7 +61,7 @@ def get_followers_groups(full_list_of_users):
         for user in l:
             code = '%s%s' % (code, 'API.groups.get({"user_id":%s}),' % str(user))
         code = '%s%s' % (code, '];')
-        r = requests.get(api_address + method + code).json()['response']
+        r = api_request(method + code, params)
         for i in r:
             if type(i) == type(True):
                 r.remove(i)
@@ -68,8 +76,8 @@ def top_groups(counted_groups):
     top_groups_list = []
     print("Топ 100 групп, в которых состоят подписчики:", end='\n')
     for group, k in counted_groups:
-        params['group_id'] = group
-        name = requests.get(api_address + 'groups.getById', params).json()['response'][0]['name']
+        params = {'group_id': group, 'access_token': ACCESS_TOKEN, 'v': VERSION}
+        name = api_request('groups.getById', params)[0]['name']
         print(name, k)
         a = {'title': name, 'count': k, 'id': group}
         top_groups_list.append(a)
@@ -87,14 +95,8 @@ def top_100_file(id_of_user):
         t.write(top_100)
 
 
-version = '5.63'
-api_address = 'https://api.vk.com/method/'
-access_token = '76e01e83604e3f9e6527ab13fb0320d4cefd4fa8424a93c3f6032c1f94096f9d09b7fd1bcd0954a9b54db'
-params = {
-    'access_token': access_token,
-    'v': version
-}
 all_followers = []
+
 print("Введите id интересующей личности:")
 user_id = input()
 
